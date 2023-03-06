d5f769db3fe52dde4a52e083aedf890896e325eb
Josh Winiarski <126107368+JoshWiniarski@users.noreply.github.com>
2023-03-06 16:52:36 -0500

    Upload: Solar System project (turtle)
    a very simple project, drawing circles at specific sizes, colors and distances from one another.

---
+++ Zenva_SolarSystem.py
@@ -0,0 +1,36 @@
+#This is a note page recording functions for turtle
+#import turtle
+from turtle import *
+
+speed(0)
+bgcolor("black")
+color('orange')
+begin_fill()
+circle(60)
+end_fill()
+penup()
+forward(100)
+
+color('gray')
+begin_fill()
+circle(20)
+end_fill()
+penup()
+forward(80)
+
+color('red')
+begin_fill()
+circle(40)
+end_fill()
+penup()
+forward(90)
+
+color('green')
+begin_fill()
+circle(30)
+end_fill()
+
+
+#keep at bottom
+#prevent window from closing
+done()
