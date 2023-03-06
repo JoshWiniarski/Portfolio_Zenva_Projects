b80ce1e65ab29694d50e448b57aadfc2531f53d9
Josh Winiarski <126107368+JoshWiniarski@users.noreply.github.com>
2023-03-06 16:57:02 -0500

    Add files via upload

---
+++ Zenva_BalloonPop.py
@@ -0,0 +1,35 @@
+#import turtle
+from turtle import *
+
+#variables
+diameter = 40
+pop_diameter = 100
+inflate_amount = 15
+
+#definitions
+def draw_balloon ():
+    color('red')
+    dot(diameter)
+    
+def inflate_balloon ():
+    global diameter
+    diameter = diameter + inflate_amount
+    draw_balloon()
+    if diameter >= pop_diameter:
+        clear()
+        diameter = 40
+        write('POP!')
+        draw_balloon()
+
+
+#code
+draw_balloon()
+
+onkey(inflate_balloon, 'w')
+listen()
+
+
+
+#keep at bottom
+#prevent window from closing
+done()
